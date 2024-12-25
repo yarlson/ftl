7475d8fa2b9c08393588ae763e31f140f09569c2
Yar Kravtsov <yarlson@gmail.com>
2024-12-25 09:32:47 +0200

    feat: Initialize Astro-based documentation site with Starlight theme

---
+++ www/astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "My Docs",
+      customCss: [
+        "@fontsource-variable/rubik",
+        "@fontsource-variable/roboto-mono",
+        "@fontsource/ibm-plex-mono/400.css",
+        "@fontsource/ibm-plex-mono/400-italic.css",
+        "@fontsource/ibm-plex-mono/500.css",
+        "@fontsource/ibm-plex-mono/600.css",
+        "@fontsource/ibm-plex-mono/700.css",
+        "./src/custom.css",
+      ],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
